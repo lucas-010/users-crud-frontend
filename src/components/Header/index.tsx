94caa4d395ceacbc3b83ac62f620b7ab5fd4d939
Lucas Pereira Ramos <lucaspereiradf07@gmail.com>
2023-08-16 02:16:37 -0300

    header concluded

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import * as S from "./styles";
+
+export const Header = () => {
+  return (
+    <S.StyledHeader>
+      <S.StyledContainerImg>
+        <S.StyledImg src="https://img.icons8.com/?size=512&id=Ej0xqjuU6l1x&format=png" />
+      </S.StyledContainerImg>
+    </S.StyledHeader>
+  );
+};
---
+++ src/components/Header/styles.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const StyledHeader = styled.header`
+  width: 100%;
+  background-color: #1e3a8a;
+`;
+
+export const StyledContainerImg = styled.div`
+  width: 80px;
+`;
+
+export const StyledImg = styled.img`
+  width: 100%;
+`;
